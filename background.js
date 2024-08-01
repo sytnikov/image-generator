d5c16fc10ead8a23c22448f4f750d7b92d4b2ecf
sytnikov <sytnikov.alexey@gmail.com>
2024-08-02 01:09:05 +0300

    welcome page, image preview

--- background.js
+++ background.js
@@ -1,3 +1,9 @@
+chrome.runtime.onInstalled.addListener((details) => {
+  if (details.reason === chrome.runtime.OnInstalledReason.INSTALL) {
+    chrome.tabs.create({ url: "https://gpt-image-generator.com/welcome/" });
+  }
+});
+
 chrome.action.onClicked.addListener(() => {
   chrome.tabs.create({ url: "page/page.html" });
 });
--- page/page.js
+++ page/page.js
@@ -1,27 +1,27 @@
-chrome.storage.local.get(["numberOfImages"], (res) => {
-  const imageCount = res.numberOfImages;
-});
-
 const imageCount = 4; // how many images we want
-let selectImageNumber = null;
-
 // to generate random number between min and max
 function getRandomNum(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
 function disableGenerateButton() {
-  document.getElementById("generate").disable = true;
+  const generateBtn = document.getElementById("generate");
+  generateBtn.disabled = true;
 }
 
 function enableGenerateButton() {
-  document.getElementById("generate").disable = false;
+  const generateBtn = document.getElementById("generate");
+  generateBtn.disabled = false;
 }
 
 function clearImageGrid() {
   // to clear the images after each generate button click...
   const imageGrid = document.getElementById("image-grid");
   imageGrid.innerHTML = "";
+  const enlargedImageContainer = document.getElementById(
+    "enlarged-image-container"
+  );
+  enlargedImageContainer.style.display = "none";
 }
 
 // generate images
@@ -52,7 +52,7 @@ async function generateImages(input) {
       );
 
       if (!response.ok) {
-        throw new Error(`HTTP error! status: ${response.status}`);
+        throw new Error(`HTTP error! Status: ${response.status}`);
       }
 
       // Get the response as a blob
@@ -66,18 +66,25 @@ async function generateImages(input) {
       const img = document.createElement("img");
       img.src = imageUrl;
       img.alt = `art-${i + 1}`;
-      img.onclick = () => downloadImage(imageUrl, i);
+      img.addEventListener("click", () => showEnlargedImage(imageUrl));
       document.getElementById("image-grid").appendChild(img);
     } catch (error) {
       console.error("Error generating image:", error);
-      alert("Unable to generate images...");
+      alert("Unable to generate an image...");
     }
   }
 
   loading.style.display = "none";
   enableGenerateButton();
+}
 
-  selectImageNumber = null;
+function showEnlargedImage(imageUrl) {
+  const enlargedImageContainer = document.getElementById(
+    "enlarged-image-container"
+  );
+  const enlargedImage = document.getElementById("enlarged-image");
+  enlargedImage.src = imageUrl;
+  enlargedImageContainer.style.display = "block";
 }
 
 function downloadImage(imageUrl, imageNumbers) {
@@ -88,7 +95,8 @@ function downloadImage(imageUrl, imageNumbers) {
 }
 
 // event listener for generate button
-document.getElementById("generate").addEventListener("click", () => {
+const generateBtn = document.getElementById("generate");
+generateBtn.addEventListener("click", () => {
   const input = document.getElementById("user-prompt").value;
   generateImages(input);
 });
