fea47cf5ff308ef71c457cf868eab230268e0fc9
sytnikov <sytnikov.alexey@gmail.com>
2024-08-06 00:08:13 +0300

    added scalability model, delete functionality fixed, icons with white background loaded

--- page/page.js
+++ page/page.js
@@ -1,107 +1,165 @@
-const imageCount = 4; // how many images we want
+const imageCount = 2 // how many images we want
+let currentImageUrl = ''
 // to generate random number between min and max
 function getRandomNum(min, max) {
-  return Math.floor(Math.random() * (max - min + 1)) + min;
+  return Math.floor(Math.random() * (max - min + 1)) + min
 }
 
 function disableGenerateButton() {
-  const generateBtn = document.getElementById("generate");
-  generateBtn.disabled = true;
+  const generateBtn = document.getElementById('generate')
+  generateBtn.disabled = true
 }
 
 function enableGenerateButton() {
-  const generateBtn = document.getElementById("generate");
-  generateBtn.disabled = false;
+  const generateBtn = document.getElementById('generate')
+  generateBtn.disabled = false
 }
 
 function clearImageGrid() {
   // to clear the images after each generate button click...
-  const imageGrid = document.getElementById("image-grid");
-  imageGrid.innerHTML = "";
+  const imageGrid = document.getElementById('image-grid')
+  imageGrid.innerHTML = ''
   const enlargedImageContainer = document.getElementById(
-    "enlarged-image-container"
-  );
-  enlargedImageContainer.style.display = "none";
+    'enlarged-image-container'
+  )
+  enlargedImageContainer.style.display = 'none'
 }
 
-// generate images
+// generate images - openjourney-v4 model
+// async function generateImages(input) {
+//   disableGenerateButton();
+//   clearImageGrid();
+
+//   const loading = document.getElementById("loading");
+//   loading.style.display = "block";
+
+//   const imageUrls = []; // array to store all the image links
+//   for (let i = 0; i < imageCount; i++) {
+//     const randomNum = getRandomNum(1, 1000);
+
+//     const prompt = `${input} ${randomNum}`;
+
+//     try {
+//       // Call the server's endpoint
+//       const response = await fetch(
+//         "https://image-generator-proxy-server.vercel.app/generate-images",
+//         {
+//           method: "POST",
+//           headers: {
+//             "Content-Type": "application/json",
+//           },
+//           body: JSON.stringify({ prompt }), // Send the prompt to the server
+//         }
+//       );
+
+//       if (!response.ok) {
+//         throw new Error(`HTTP error! Status: ${response.status}`);
+//       }
+
+//       // Get the response as a blob
+//       const result = await response.blob();
+
+//       // Create an object URL for the image blob
+//       const imageUrl = URL.createObjectURL(result);
+//       imageUrls.push(imageUrl);
+
+//       // Creating image elements
+//       const img = document.createElement("img");
+//       img.src = imageUrl;
+//       img.alt = `art-${i + 1}`;
+//       img.addEventListener("click", () => showEnlargedImage(imageUrl, i));
+//       document.getElementById("image-grid").appendChild(img);
+//     } catch (error) {
+//       console.error("Error generating image:", error);
+//       alert("Unable to generate an image...");
+//     }
+//   }
+
+//   loading.style.display = "none";
+//   enableGenerateButton();
+// }
+
+// generate images - stability model
 async function generateImages(input) {
-  disableGenerateButton();
-  clearImageGrid();
+  disableGenerateButton()
+  clearImageGrid()
 
-  const loading = document.getElementById("loading");
-  loading.style.display = "block";
+  const loading = document.getElementById('loading')
+  loading.style.display = 'block'
 
-  const imageUrls = []; // array to store all the image links
+  const imageUrls = [] // array to store all the image links
   for (let i = 0; i < imageCount; i++) {
-    const randomNum = getRandomNum(1, 1000);
+    const randomNum = getRandomNum(1, 1000)
 
-    const prompt = `${input} ${randomNum}`;
+    const prompt = `${input} ${randomNum}`
 
     try {
       // Call the server's endpoint
       const response = await fetch(
-        "https://image-generator-proxy-server.vercel.app/generate-images",
+        'https://image-generator-proxy-server.vercel.app/stability-model',
         {
-          method: "POST",
+          method: 'POST',
           headers: {
-            "Content-Type": "application/json",
+            'Content-Type': 'application/json',
           },
           body: JSON.stringify({ prompt }), // Send the prompt to the server
         }
-      );
+      )
 
       if (!response.ok) {
-        throw new Error(`HTTP error! Status: ${response.status}`);
+        throw new Error(`HTTP error! Status: ${response.status}`)
       }
 
-      // Get the response as a blob
-      const result = await response.blob();
-
-      // Create an object URL for the image blob
-      const imageUrl = URL.createObjectURL(result);
-      imageUrls.push(imageUrl);
+      const result = await response.json()
+      const base64String = result[0].base64
+      const imageUrl = `data:image/png;base64,${base64String}`
+      imageUrls.push(imageUrl)
 
       // Creating image elements
-      const img = document.createElement("img");
-      img.src = imageUrl;
-      img.alt = `art-${i + 1}`;
-      img.addEventListener("click", () => showEnlargedImage(imageUrl, i));
-      document.getElementById("image-grid").appendChild(img);
+      const img = document.createElement('img')
+      img.src = imageUrl
+      img.alt = `art-${i + 1}`
+      img.addEventListener('click', () => showEnlargedImage(imageUrl, i))
+      document.getElementById('image-grid').appendChild(img)
     } catch (error) {
-      console.error("Error generating image:", error);
-      alert("Unable to generate an image...");
+      console.error('Error generating image:', error)
+      alert('Unable to generate an image...')
     }
   }
 
-  loading.style.display = "none";
-  enableGenerateButton();
+  loading.style.display = 'none'
+  enableGenerateButton()
 }
 
 function showEnlargedImage(imageUrl, imageNumber) {
   const enlargedImageContainer = document.getElementById(
-    "enlarged-image-container"
-  );
-  const enlargedImage = document.getElementById("enlarged-image");
-  enlargedImage.src = imageUrl;
-  enlargedImageContainer.style.display = "block";
-
-  const downloadBtn = document.getElementById("download-btn");
-  downloadBtn.addEventListener("click", () =>
-    downloadImage(imageUrl, imageNumber)
-  );
+    'enlarged-image-container'
+  )
+  const enlargedImage = document.getElementById('enlarged-image')
+  enlargedImage.src = imageUrl
+  enlargedImageContainer.style.display = 'block'
+  currentImageUrl = imageUrl
 }
+
+function downloadImage() {
+  if (currentImageUrl) {
+    // Check if there is a current image URL
+    const link = document.createElement('a')
+    link.href = currentImageUrl
+    link.download = `image-${new Date().getTime()}.png` // Use a timestamp to avoid naming conflicts
+    link.click()
+  } else {
+    alert('No image to download')
+  }
+}
+
+// Event listener for the download button
+const downloadBtn = document.getElementById('download-btn')
+downloadBtn.addEventListener('click', () => downloadImage())
 
 // event listener for generate button
-const generateBtn = document.getElementById("generate");
-generateBtn.addEventListener("click", () => {
-  const input = document.getElementById("user-prompt").value;
-  generateImages(input);
-});
-
-function downloadImage(imageUrl, imageNumber) {
-  const link = document.createElement("a");
-  link.href = imageUrl;
-  link.download = `image-${imageNumber + 1}.png`;
-  link.click();
-}
+const generateBtn = document.getElementById('generate')
+generateBtn.addEventListener('click', () => {
+  const input = document.getElementById('user-prompt').value
+  generateImages(input)
+})
